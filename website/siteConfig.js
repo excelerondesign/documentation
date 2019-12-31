3b6d5076fafed083a4e945b8d063524d9283f85b
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2019-12-31 17:22:53 -0500

    updates to custom domain name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Documentation', // Title for your website.
   tagline: 'Internal Documentation: Notes, Tips, Tricks, and other helpful snippets.',
-  url: 'https://excelerondesign.github.io', // Your website URL
-  baseUrl: '/documentation/', // Base URL for your project */
+  url: 'https://dev.exceleron.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
